84315269e3412bb9c8a815f15052e096c8ae2d5c
Barry van Oudtshoorn <bvanoudtshoorn@gmail.com>
2010-09-29 12:25:30 +0800

    * Multi-parameter values (eg, 0 0 4px 0) will now be simplified (0 0 4px 0 => 0 0 4px) * HTML colour names (black, aliceblue, aqua) are replaced by their hex equivalents (when this is shorter) * "!important" now sits flush, not padded with a space

--- CSSMin.java
+++ CSSMin.java
@@ -235,7 +235,7 @@ class Selector {
 	 * @param properties The array to be sorted.
 	 */
 	private void sortProperties(Property[] properties) {
-		Arrays.sort(properties);
+		//Arrays.sort(properties);
 	}
 }
 
@@ -334,7 +334,11 @@ class Property implements Comparable<Property> {
 	}
 	
 	private String simplifyColours(String contents) {
-		contents = contents.replaceAll("rgba\\(0,0,0,0\\)", "transparent");
+		// This replacement, although it results in a smaller uncompressed file,
+		// actually makes the gzipped file bigger -- people tend to use rgba(0,0,0,0.x)
+		// quite a lot, which means that rgba(0,0,0,0) has its first eight or so characters
+		// compressed really efficiently; much more so than "transparent".
+		//contents = contents.replaceAll("rgba\\(0,0,0,0\\)", "transparent");
 		
 		return simplifyRGBColours(contents);
 	}
@@ -385,11 +389,15 @@ class Part {
 	}
 	
 	private void simplify() {
+		// !important doesn't need to be spaced
+		this.contents = this.contents.replaceAll(" !important", "!important");
+		
 		// Replace 0in, 0cm, etc. with just 0
 		this.contents = this.contents.replaceAll("(\\s)(0)(px|em|%|in|cm|mm|pc|pt|ex)", "$1$2");
 		
 		// Replace 0.6 with .6
-		this.contents = this.contents.replaceAll("(\\s)0+\\.(\\d+)", "$1.$2");
+		// Disabled, as it actually makes compression worse! People use rgba(0,0,0,0) and rgba(0,0,0,0.x) a lot.
+		//this.contents = this.contents.replaceAll("(\\s)0+\\.(\\d+)", "$1.$2");
 		
 		this.contents = this.contents.trim();
 		
@@ -398,7 +406,57 @@ class Part {
 		if (this.contents.equals("0 0 0")) this.contents = "0";
 		if (this.contents.equals("0 0")) this.contents = "0";
 		
+		// Simplify multiple-parameter properties
+		simplifyParameters();
+		
+		// Simplify colours
+		simplifyColourNames();
 		simplifyHexColours();
+	}
+	
+	private void simplifyParameters() {
+		StringBuffer newContents = new StringBuffer();
+		
+		String[] params = this.contents.split(" ");
+		if (params.length == 4) {
+			// We can drop off the fourth item if the second and fourth items match
+			// ie turn 3px 0 3px 0 into 3px 0 3px
+			if (params[1].equalsIgnoreCase(params[3])) {
+				params = Arrays.copyOf(params, 3);
+			}
+		}
+		if (params.length == 3) {
+			// We can drop off the third item if the first and third items match
+			// ie turn 3px 0 3px into 3px 0
+			if (params[0].equalsIgnoreCase(params[2])) {
+				params = Arrays.copyOf(params, 2);
+			}
+		}
+		if (params.length == 2) {
+			// We can drop off the second item if the first and second items match
+			// ie turn 3px 3px into 3px
+			if (params[0].equalsIgnoreCase(params[1])) {
+				params = Arrays.copyOf(params, 1);
+			}
+		}
+		
+		for (int i = 0; i < params.length; i++) {
+			newContents.append(params[i] + " ");
+		}
+		newContents.deleteCharAt(newContents.length() - 1); // Delete the trailing space
+		
+		this.contents = newContents.toString();
+	}
+	
+	private void simplifyColourNames() {
+		String lcContents = this.contents.toLowerCase();
+		
+		for (int i = 0; i < Constants.htmlColourNames.length; i++) {
+			if (lcContents.equals(Constants.htmlColourNames[i])) {
+				this.contents = Constants.htmlColourValues[i];
+				break;
+			}
+		}
 	}
 	
 	private void simplifyHexColours() {
@@ -426,4 +484,292 @@ class Part {
 	public String toString() {
 		return this.contents;
 	}
+}
+
+class Constants {
+	static final String[] htmlColourNames = {
+		"aliceblue",
+		"antiquewhite",
+		"aqua",
+		"aquamarine",
+		"azure",
+		"beige",
+		"bisque",
+		"black",
+		"blanchedalmond",
+		"blue",
+		"blueviolet",
+		"brown",
+		"burlywood",
+		"cadetblue",
+		"chartreuse",
+		"chocolate",
+		"coral",
+		"cornflowerblue",
+		"cornsilk",
+		"crimson",
+		"cyan",
+		"darkblue",
+		"darkcyan",
+		"darkgoldenrod",
+		"darkgray",
+		"darkgreen",
+		"darkkhaki",
+		"darkmagenta",
+		"darkolivegreen",
+		"darkorange",
+		"darkorchid",
+		"darkred",
+		"darksalmon",
+		"darkseagreen",
+		"darkslateblue",
+		"darkslategray",
+		"darkturquoise",
+		"darkviolet",
+		"deeppink",
+		"deepskyblue",
+		"dimgray",
+		"dodgerblue",
+		"firebrick",
+		"floralwhite",
+		"forestgreen",
+		"fuchsia",
+		"gainsboro",
+		"ghostwhite",
+		"gold",
+		"goldenrod",
+		"gray",
+		"green",
+		"greenyellow",
+		"honeydew",
+		"hotpink",
+		"indianred ",
+		"indigo ",
+		"ivory",
+		"khaki",
+		"lavender",
+		"lavenderblush",
+		"lawngreen",
+		"lemonchiffon",
+		"lightblue",
+		"lightcoral",
+		"lightcyan",
+		"lightgoldenrodyellow",
+		"lightgrey",
+		"lightgreen",
+		"lightpink",
+		"lightsalmon",
+		"lightseagreen",
+		"lightskyblue",
+		"lightslategray",
+		"lightsteelblue",
+		"lightyellow",
+		"lime",
+		"limegreen",
+		"linen",
+		"magenta",
+		"maroon",
+		"mediumaquamarine",
+		"mediumblue",
+		"mediumorchid",
+		"mediumpurple",
+		"mediumseagreen",
+		"mediumslateblue",
+		"mediumspringgreen",
+		"mediumturquoise",
+		"mediumvioletred",
+		"midnightblue",
+		"mintcream",
+		"mistyrose",
+		"moccasin",
+		"navajowhite",
+		"navy",
+		"oldlace",
+		"olive",
+		"olivedrab",
+		"orange",
+		"orangered",
+		"orchid",
+		"palegoldenrod",
+		"palegreen",
+		"paleturquoise",
+		"palevioletred",
+		"papayawhip",
+		"peachpuff",
+		"peru",
+		"pink",
+		"plum",
+		"powderblue",
+		"purple",
+		"red",
+		"rosybrown",
+		"royalblue",
+		"saddlebrown",
+		"salmon",
+		"sandybrown",
+		"seagreen",
+		"seashell",
+		"sienna",
+		"silver",
+		"skyblue",
+		"slateblue",
+		"slategray",
+		"snow",
+		"springgreen",
+		"steelblue",
+		"tan",
+		"teal",
+		"thistle",
+		"tomato",
+		"turquoise",
+		"violet",
+		"wheat",
+		"white",
+		"whitesmoke",
+		"yellow",
+		"yellowgreen"
+	};
+	
+	static final String[] htmlColourValues = {
+		"#f0f8ff",
+		"#faebd7",
+		"#00ffff",
+		"#7fffd4",
+		"#f0ffff",
+		"#f5f5dc",
+		"#ffe4c4",
+		"#000000",
+		"#ffebcd",
+		"#0000ff",
+		"#8a2be2",
+		"#a52a2a",
+		"#deb887",
+		"#5f9ea0",
+		"#7fff00",
+		"#d2691e",
+		"#ff7f50",
+		"#6495ed",
+		"#fff8dc",
+		"#dc143c",
+		"#00ffff",
+		"#00008b",
+		"#008b8b",
+		"#b8860b",
+		"#a9a9a9",
+		"#006400",
+		"#bdb76b",
+		"#8b008b",
+		"#556b2f",
+		"#ff8c00",
+		"#9932cc",
+		"#8b0000",
+		"#e9967a",
+		"#8fbc8f",
+		"#483d8b",
+		"#2f4f4f",
+		"#00ced1",
+		"#9400d3",
+		"#ff1493",
+		"#00bfff",
+		"#696969",
+		"#1e90ff",
+		"#b22222",
+		"#fffaf0",
+		"#228b22",
+		"#ff00ff",
+		"#dcdcdc",
+		"#f8f8ff",
+		"#ffd700",
+		"#daa520",
+		"#808080",
+		"#008000",
+		"#adff2f",
+		"#f0fff0",
+		"#ff69b4",
+		"#cd5c5c",
+		"#4b0082",
+		"#fffff0",
+		"#f0e68c",
+		"#e6e6fa",
+		"#fff0f5",
+		"#7cfc00",
+		"#fffacd",
+		"#add8e6",
+		"#f08080",
+		"#e0ffff",
+		"#fafad2",
+		"#d3d3d3",
+		"#90ee90",
+		"#ffb6c1",
+		"#ffa07a",
+		"#20b2aa",
+		"#87cefa",
+		"#778899",
+		"#b0c4de",
+		"#ffffe0",
+		"#00ff00",
+		"#32cd32",
+		"#faf0e6",
+		"#ff00ff",
+		"#800000",
+		"#66cdaa",
+		"#0000cd",
+		"#ba55d3",
+		"#9370d8",
+		"#3cb371",
+		"#7b68ee",
+		"#00fa9a",
+		"#48d1cc",
+		"#c71585",
+		"#191970",
+		"#f5fffa",
+		"#ffe4e1",
+		"#ffe4b5",
+		"#ffdead",
+		"#000080",
+		"#fdf5e6",
+		"#808000",
+		"#6b8e23",
+		"#ffa500",
+		"#ff4500",
+		"#da70d6",
+		"#eee8aa",
+		"#98fb98",
+		"#afeeee",
+		"#d87093",
+		"#ffefd5",
+		"#ffdab9",
+		"#cd853f",
+		"#ffc0cb",
+		"#dda0dd",
+		"#b0e0e6",
+		"#800080",
+		"#ff0000",
+		"#bc8f8f",
+		"#4169e1",
+		"#8b4513",
+		"#fa8072",
+		"#f4a460",
+		"#2e8b57",
+		"#fff5ee",
+		"#a0522d",
+		"#c0c0c0",
+		"#87ceeb",
+		"#6a5acd",
+		"#708090",
+		"#fffafa",
+		"#00ff7f",
+		"#4682b4",
+		"#d2b48c",
+		"#008080",
+		"#d8bfd8",
+		"#ff6347",
+		"#40e0d0",
+		"#ee82ee",
+		"#f5deb3",
+		"#ffffff",
+		"#f5f5f5",
+		"#ffff00",
+		"#9acd32"
+	};
 }
